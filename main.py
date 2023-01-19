7d8038e39f887af564e121c60f6423aced0d1444
CousindeYOU <axcol.ellet@gmail.com>
2023-01-19 09:06:09 +0100

    Main

--- main.py
+++ main.py
@@ -69,7 +69,6 @@ def main():
       stk = [discord.utils.get(message.guild.stickers, name='bonk')]
       await message.channel.send(f"Go to horny jail <@{ayer_id}>", stickers=stk)
 
-
     if message.content.startswith('>help'):
       await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=">help"))
       await message.channel.send(f"Utilise `>stats <pseudo> <plateforme>` pour avoir les stats d'un joueur Apex {message.author.mention} !")
@@ -91,6 +90,38 @@ def main():
           await message.channel.send(f"Utilise `>stats <pseudo> <PC/PS4/X1>`` pour avoir les stats d'un joueur Apex {message.author.mention} !")
       else:
         await message.channel.send(embed=utils.get_stats(player_name=player_name, platform=platform))
+
+    #CHANGEMENTS DE COUSIN -- A TESTER
+
+    #CHANGEMENT 1: ego lt -- devrait être fonctionnel
+    if (message.content.find(
+        'ego') != -1) and message.author.id == 445835527413956608:
+      ltx_id = 882719651119898656
+      #Tests momentanés, remettre LT une fois testé
+      cousin_id = 445835527413956608
+      await message.channel.send(f"T'es full ego lt <@{ltx_id}>")
+
+    #CHANGEMENT 2: Taille Bite bot -- des mots augmentent la taille de bite et d'autres la réduise, faut garder la recette secrète
+    #">taille_bite" sert à s'intégrer à l'enregistrement de taille de bite, ">stats_bite" servira à prendre des nouvelles sur sa taille de bite
+    if (message.content.startswith('>taille_bite') != -1):
+      #A faire: une boucle pour ajouter nouvel id (au lieu d'author1) au json à chaque fois qu'un nouvel utilisateur fait >taille_bite
+      author1 = message.author.id
+      taille_peni = 15 #taille de base pour tous
+      await message.channel.send(f"Ta bite fait 15 cm <@{author1}>, beau péni")
+      
+      
+      #Pour prendre les stats de la bite (si tu trouve une commande plus agréable à écrire que >stats_bite hésite pas à modif)
+    if (message.content.startswith('>stats_bite') != -1):
+      taille_peni = 
+      if (taille_peni > 15):
+        await message.channel.send(f"Beau péni <@{author1}>")
+      elif (taille_peni > 12):
+        await message.channel.send(f"Petit péni <@{author1}>")
+      else:
+        await message.channel.send(f"Micro péni <@{author1}>")
+
+
+
 
 
   keep_alive()
